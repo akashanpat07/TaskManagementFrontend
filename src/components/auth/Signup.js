e188b6d32db277df04210f333dda9a780bfb1272
Akash Anpat <54320309+akashanpat07@users.noreply.github.com>
2024-10-04 01:05:25 +0530

    Update Signup.js

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -17,7 +17,7 @@ function Signup({isAuthenticated, setIsAuthenticated}) {
     e.preventDefault();
 
     try {
-      const response = await axios.post('https://steadfast-liberation.railway.app/api/auth/signup', {username, password});
+      const response = await axios.post('taskmanagementbackend-production-2202.up.railway.app/api/auth/signup', {username, password});
       sessionStorage.setItem('token', response.data.token);
       sessionStorage.setItem('name', response.data.username);
     
